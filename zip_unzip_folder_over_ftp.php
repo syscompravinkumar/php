9091b7d990126ed8de5657d47bada089264b38c5
syscompravinkumar <syscom.pravinkumar@gmail.com>
2017-04-13 16:12:41 +0530

    Update zip_unzip_folder_over_ftp.php

--- zip_unzip_folder_over_ftp.php
+++ zip_unzip_folder_over_ftp.php
@@ -1,6 +1,6 @@
 <?php
 //----- zip the folder
-system('umask 755'); system('zip -r ../archive.zip ../design.ultimenews.net');
+system('umask 755'); system('zip -r file_location_zip_store.zip folder_location_that_to_zipped');
 //---- unzip the file
 system('umask 755'); system('unzip zip_file_location_as_this_file.zip');
 
