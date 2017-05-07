547ca87d388f9227d8fc6cad3e02ebd6af9482f6
syscompravinkumar <syscom.pravinkumar@gmail.com>
2017-05-07 17:54:00 +0530

    Update zip_unzip_folder_over_ftp.php

--- zip_unzip_folder_over_ftp.php
+++ zip_unzip_folder_over_ftp.php
@@ -3,5 +3,8 @@
 system('umask 755'); system('zip -r file_location_zip_store.zip folder_location_that_to_zipped');
 //---- unzip the file
 system('umask 755'); system('unzip zip_file_location_as_this_file.zip');
-
+//---- dump database
+system('mysqldump -uname -ppassword dbname > download_database.sql');
+//---- upload database
+system('mysql -uname -ppassword dbname < source_file_path.sql');
 ?>
